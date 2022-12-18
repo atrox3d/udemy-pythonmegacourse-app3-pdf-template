a39b36cbcb01f4a58468b8b0644bb0ec000d4b8a
atrox3d <atrox3d@gmail.com>
2022-12-18 09:38:57 +0100

    day 24: from csv to pdf

--- main.py
+++ main.py
@@ -1,13 +1,38 @@
 from fpdf import FPDF
 import pandas as pd
+import webbrowser
+
+
+def gettcoords(pdf):
+    x = round(pdf.get_x(), 2)
+    y = round(pdf.get_y(), 2)
+    w = round(pdf.w - (pdf.l_margin + pdf.r_margin), 2)
+    h = round(pdf.h - (pdf.t_margin + pdf.b_margin), 2)
+    return x, y, w, h
+
+
+def printcoords(pdf):
+    x, y, w, h = gettcoords(pdf)
+    print(f"{x=} {y=} {w=} {h=} ")
 
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
-pdf.add_page()
-pdf.set_font("Times", style="B", size=12)
-pdf.cell(w=0, txt="Test", h=12, align="L", border=0, ln=1)
-pdf.cell(w=0, txt="Test", h=12, align="L", border=0)
-pdf.ln()
-pdf.cell(w=0, txt="Test", h=12, align="L", border=0)
-pdf.output("test.pdf")
+df = pd.read_csv("topics.csv")
+
+for index, row in df.iterrows():
+
+    pdf.add_page()
+    pdf.set_font("Times", style="B", size=24)
+    # pdf.set_text_color(100, 100, 100)
+
+    print(index, row.Order, row.Topic, row.Pages)
+    printcoords(pdf)
+    pdf.cell(w=0, txt=row.Topic, h=12, align="L", border=0, ln=1)
+    printcoords(pdf)
+
+    x, y, w, h = gettcoords(pdf)
+    pdf.line(x, y, x + w, y)
+
+pdf.output("output.pdf")
+webbrowser.open("output.pdf")
