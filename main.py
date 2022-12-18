48e7416a2d004d6916023783eab226e63a220107
atrox3d <atrox3d@gmail.com>
2022-12-18 14:07:18 +0100

    day 24: experiment: add parametric decorator to FPDF.footer callback

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fpdf import FPDF
 import pandas as pd
 import webbrowser
+import types
 
 
 def gettcoords(pdf):
@@ -16,11 +17,28 @@ def printcoords(pdf):
     print(f"{x=} {y=} {w=} {h=} ")
 
 
+
+
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
 df = pd.read_csv("topics.csv")
 
+
+def footer_decorator(topic):                    # saves topic of current loop
+    def decorator(func):                        # decorates footer
+        def wrapper(self):                      # wraps func, passes on self
+            func(self, topic)                   # call func with self, topic
+        return wrapper
+    return decorator
+
+
 for index, row in df.iterrows():
+
+    @footer_decorator(row.Topic)                # parametric decorator with current topic
+    def footer(self, topic):                    # method to monkey patch
+        print("footer", self, topic)
+
+    pdf.footer = types.MethodType(footer, pdf)  # monkey patch method
 
     pdf.add_page()
     pdf.set_font("Times", style="B", size=24)
@@ -35,6 +53,7 @@ for index, row in df.iterrows():
     pdf.line(x, y, x + w, y)
     for page in range(row.Pages - 1):
         pdf.add_page()
+
 
 pdf.output("output.pdf")
 webbrowser.open("output.pdf")
